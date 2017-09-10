b78a375dc5f49564d7777105b3c2a5b693afc8ee
JacekMistrzuKoder <jacek301@gmail.com>
2017-09-10 22:46:45 +0200

    egyptians Fractions Kata Fun Update 3

--- src/test/java/com/java8/mySamples/SomeEgyptianFractionsKata.java
+++ src/test/java/com/java8/mySamples/SomeEgyptianFractionsKata.java
@@ -5,12 +5,14 @@ import java.math.BigInteger;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 
 public class SomeEgyptianFractionsKata {
-	
-	private final static double ACCEPTABLE_TOLERANCE = 7.65790E-9 ;
+
+//	private final static double ACCEPTABLE_TOLERANCE = 7.65790E-9;
+	private final static double ACCEPTABLE_TOLERANCE = 7.65790E-9;
 
 	@Test
 	public void test() {
@@ -18,9 +20,10 @@ public class SomeEgyptianFractionsKata {
 		// System.out.println(convertDecimalToFraction(0.00395256916996047));
 
 		// System.out.println(convertDecimalToFraction(0.00000155268413));
-//		 System.out.println(convertDecimalToFraction(0.0000015526841250));
-//		 System.out.println(convertDecimalToFraction(6.00000000000000));
-//		 System.out.println(convertDecimalToFraction(0.0833333333333333));
+		// System.out.println(convertDecimalToFraction(0.0000015526841250));
+		// System.out.println(convertDecimalToFraction(6.00000000000000));
+		// System.out.println(convertDecimalToFraction(0.0833333333333333));
+		// System.out.println(convertDecimalToFraction(6));
 
 		// 0.00909090909090909
 
@@ -29,18 +32,29 @@ public class SomeEgyptianFractionsKata {
 		// int[] numbers = toFractionPos(new BigDecimal("0.0090456"));
 		//
 		// System.out.println(numbers[0]+"/"+numbers[1]);
+		
+		long timeStart =  System.currentTimeMillis();
+//		
+//		System.out.println(timeStart/1000 % 60);
+		
+		String fractionsList;
 
-		 decompose("21", "23");
-//		 decompose("12", "4");
-//		 decompose("0", "2");
-//		 decompose("12", "2");
-//		 decompose("9", "10");
-//		 decompose("34", "645");
+//		 fractionsList = decompose("21", "23");
+//		fractionsList =	 decompose("12", "4");
+//		fractionsList =  decompose("0", "2");
+//		fractionsList =  decompose("12", "2");
+//		fractionsList =	 decompose("9", "10");
+//		fractionsList =	 decompose("34", "645");
 		// decompose("3", "4");
 		// decompose("5", "8");
-//		 decompose("13", "12");
-//		decompose("24", "12");
-//		decompose("11", "12");
+//		decompose("13", "12");
+		// decompose("24", "12");
+//		 decompose("11", "12");
+		 fractionsList =  decompose("66", "100");
+		 
+		 System.out.println(fractionsList);
+		 
+		 System.out.println((System.currentTimeMillis() - timeStart) /1000 % 60);
 
 	}
 
@@ -54,17 +68,17 @@ public class SomeEgyptianFractionsKata {
 		BigDecimal currentDenominator = new BigDecimal(2);
 		BigDecimal currentFractionSum = new BigDecimal(0);
 
-		List<BigDecimal> ListOfEgyptianFractions = new ArrayList<>();
+		List<BigDecimal> listOfEgyptianFractionsInDecimal = new ArrayList<>();
 
-		if (numerator.remainder(denominator).compareTo(BigDecimal.ZERO) == 0 && numerator.compareTo(BigDecimal.ZERO)  != 0) {
+		if (numerator.remainder(denominator).compareTo(BigDecimal.ZERO) == 0
+				&& numerator.compareTo(BigDecimal.ZERO) != 0) {
 
-			ListOfEgyptianFractions.add(decimalFraction.setScale(0));
-			System.out.println("currentFractionSum: " + ListOfEgyptianFractions);
+			listOfEgyptianFractionsInDecimal.add(decimalFraction.setScale(0));
 
-			return "";
+			return listOfEgyptianFractionsInDecimal.toString();
 		}
-		
-		BigDecimal diffrentCurrentFractionAndCurrentFractionsSum;
+
+		BigDecimal currentFractionAndCurrentFractionsSumDiffrence;
 
 		do {
 
@@ -73,7 +87,7 @@ public class SomeEgyptianFractionsKata {
 
 			if (currentFractionSum.compareTo(decimalFraction) <= 0) {
 
-				ListOfEgyptianFractions.add(tmpFraction);
+				listOfEgyptianFractionsInDecimal.add(tmpFraction);
 
 			}
 
@@ -83,29 +97,44 @@ public class SomeEgyptianFractionsKata {
 			}
 
 			currentDenominator = currentDenominator.add(BigDecimal.ONE);
+			currentFractionAndCurrentFractionsSumDiffrence = decimalFraction.subtract(currentFractionSum);
 
-			System.out.println("currentFractionSum: " + currentFractionSum);
-			System.out.println("   decimalFraction: " + decimalFraction);
-			System.out.println("currentDenominator: " + currentDenominator);
-			System.out.println("ListOfEgyptianFractions: " + ListOfEgyptianFractions);
-			
-			 diffrentCurrentFractionAndCurrentFractionsSum = decimalFraction.subtract(currentFractionSum);
-			
-						System.out.println("diffrentCurrentFractionAndCurrentFractionSum: " + diffrentCurrentFractionAndCurrentFractionsSum);
-						
-//						diffrentCurrentFractionAndCurrentFractionSum.compareTo(val)
-			
+		} while (currentFractionAndCurrentFractionsSumDiffrence.compareTo(new BigDecimal(ACCEPTABLE_TOLERANCE)) > 0);
 
-//		} while (currentFractionSum.compareTo(decimalFraction) < 0
-//				&& currentDenominator.compareTo(new BigDecimal(1000000)) != 0);
-//	} while (currentFractionSum.compareTo(decimalFraction) < 0
-//			&& diffrentCurrentFractionAndCurrentFractionsSum.compareTo(new BigDecimal(ACCEPTABLE_TOLERANCE)) > 0 );
-	} while (diffrentCurrentFractionAndCurrentFractionsSum.compareTo(new BigDecimal(ACCEPTABLE_TOLERANCE)) > 0 );
-		// } while (currentDenominator.compareTo(new BigDecimal(644049)) < 0);
+		List<String> egyptianfractions = listOfEgyptianFractionsInDecimal.stream()
+				.map(df -> convertDecimalToFraction(df.doubleValue()))
+				.collect(Collectors.toList());
 
-		System.out.println(decimalFraction.toString());
+		return egyptianfractions.toString();
+	}
 
-		return null;
+	static private String convertDecimalToFraction(double x) {
+		if (x < 0) {
+			return "-" + convertDecimalToFraction(-x);
+		}
+		double tolerance = 1.0E-6;
+		double h1 = 1;
+		double h2 = 0;
+
+		double k1 = 0;
+		double k2 = 1;
+		double b = x;
+		do {
+			double a = Math.floor(b);
+			double aux = h1;
+
+			h1 = a * h1 + h2;
+			h2 = aux;
+
+			aux = k1;
+			k1 = a * k1 + k2;
+
+			k2 = aux;
+			b = 1 / (b - a);
+
+		} while (Math.abs(x - h1 / k1) > x * tolerance);
+
+		return (int) h1 + "/" + (int) k1;
 	}
 
 	static int[] toFractionPos(BigDecimal x) {
@@ -137,9 +166,9 @@ public class SomeEgyptianFractionsKata {
 		return rf;
 	}
 
-	static private String convertDecimalToFraction(double x) {
+	static private String convertDecimalToFractionDebug(double x) {
 		if (x < 0) {
-			return "-" + convertDecimalToFraction(-x);
+			return "-" + convertDecimalToFractionDebug(-x);
 		}
 		double tolerance = 1.0E-6;
 		double h1 = 1;
