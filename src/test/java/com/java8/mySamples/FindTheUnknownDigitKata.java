e0231a9674e134d4e409f33eaf8c8d92f9ae72af
JacekMistrzuKoder <jacek301@gmail.com>
2017-09-14 02:44:29 +0200

    find the Unknown Digit Kata begin

---
+++ src/test/java/com/java8/mySamples/FindTheUnknownDigitKata.java
@@ -0,0 +1,27 @@
+package com.java8.mySamples;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FindTheUnknownDigitKata {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	
+	
+	public static int solveExpression( final String expression ) {
+		int missingDigit = -1;
+
+    //Write code to determine the missing digit or unknown rune
+    //Expression will always be in the form
+    //(number)[opperator](number)=(number)
+    //Unknown digit will not be the same as any other digits used in expression
+    
+		return missingDigit;
+	}
+
+}
