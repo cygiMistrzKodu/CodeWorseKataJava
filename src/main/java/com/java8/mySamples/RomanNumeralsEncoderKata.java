364550bddd9c389385df8f8198b5237c620f8406
JacekMistrzuKoder <jacek301@gmail.com>
2017-09-26 01:05:18 +0200

    Roman Numerals Encoder Kata 2 Progress

--- src/main/java/com/java8/mySamples/RomanNumeralsEncoderKata.java
+++ src/main/java/com/java8/mySamples/RomanNumeralsEncoderKata.java
@@ -1,9 +1,75 @@
 package com.java8.mySamples;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
 public class RomanNumeralsEncoderKata {
-    
+
+    Map<Integer, String> romanNumberSymbol = new HashMap<>();
+
+    String romanNumber = "";
+
     public String solution(int n) {
-        return "";
+
+	initilizeRomanNumberMap();
+
+	romanNumber = "";
+	
+	
+	
+	if (n >= 20 && n <= 30) {
+
+	    IntStream.rangeClosed(1, n/10).forEach(index -> romanNumber += romanNumberSymbol.get(10));
+	    
+	    n = n - 20;
+
+	}
+	
+
+	if (n >= 10 && n <= 19) {
+
+	    romanNumber += romanNumberSymbol.get(10);
+	    n = n - 10;
+
+	}
+
+	if (n >= 5 && n <= 8) {
+
+	    romanNumber += romanNumberSymbol.get(5);
+	    n = n - 5;
+
+	}
+
+	if (n <= 3) {
+
+	    IntStream.rangeClosed(1, n).forEach(index -> romanNumber += romanNumberSymbol.get(1));
+
+	}
+
+	if (n == 4) {
+
+	    romanNumber += romanNumberSymbol.get(1);
+	    romanNumber += romanNumberSymbol.get(5);
+
+	}
+
+	if (n == 9) {
+	    romanNumber += romanNumberSymbol.get(1);
+	    romanNumber += romanNumberSymbol.get(10);
+	}
+
+	return romanNumber;
+    }
+
+    private void initilizeRomanNumberMap() {
+	romanNumberSymbol.put(1, "I");
+	romanNumberSymbol.put(5, "V");
+	romanNumberSymbol.put(10, "X");
+	romanNumberSymbol.put(50, "L");
+	romanNumberSymbol.put(100, "C");
+	romanNumberSymbol.put(500, "D");
+	romanNumberSymbol.put(1000, "M");
     }
 
 }
--- src/test/java/com/java8/mySamples/RomanNumeralsEncoderKataTest.java
+++ src/test/java/com/java8/mySamples/RomanNumeralsEncoderKataTest.java
@@ -8,17 +8,95 @@ import org.junit.Test;
 public class RomanNumeralsEncoderKataTest {
     
     private RomanNumeralsEncoderKata conversion = new RomanNumeralsEncoderKata();
-
+    
     @Test
-    public void shouldCovertToRoman() {
-        assertEquals("solution(1) should equal to I", "I", conversion.solution(1));
-        assertEquals("solution(4) should equal to IV", "IV", conversion.solution(4));
-        assertEquals("solution(6) should equal to VI", "VI", conversion.solution(6));
+    public void one_Equals_I() {
+	 assertEquals("solution(1) should equal to I", "I", conversion.solution(1));
     }
     
     @Test
-    public void test() {
-	fail("Not yet implemented");
+    public void two_Equals_II() {
+	 assertEquals("solution(1) should equal to II", "II", conversion.solution(2));
+    }
+    
+    @Test
+    public void two_Equals_III() {
+	 assertEquals("solution(1) should equal to III", "III", conversion.solution(3));
+    }
+    
+    @Test
+    public void four_Equals_IV() {
+	 assertEquals("solution(4) should equal to IV", "IV", conversion.solution(4));
+    }
+    
+    @Test
+    public void six_Equals_VI() {
+	assertEquals("solution(6) should equal to VI", "VI", conversion.solution(6));
+    }
+    
+    @Test
+    public void seven_Equals_VII() {
+	assertEquals("solution(7) should equal to VII", "VII", conversion.solution(7));
+    }
+    
+    @Test
+    public void seven_Equals_VIII() {
+	assertEquals("solution(8) should equal to VIII", "VIII", conversion.solution(8));
+    }
+    
+    @Test
+    public void nine_Equals_IX() {
+	assertEquals("solution(9) should equal to IX", "IX", conversion.solution(9));
+    }
+    
+    @Test
+    public void ten_Equals_X() {
+	assertEquals("solution(10) should equal to X", "X", conversion.solution(10));
+    }
+    
+    @Test
+    public void _13_Equals_XIII() {
+	assertEquals("solution(13) should equal to XIII", "XIII", conversion.solution(13));
+    }
+    
+    @Test
+    public void _14_Equals_XIV() {
+	assertEquals("solution(14) should equal to XIV", "XIV", conversion.solution(14));
+    }
+    
+    @Test
+    public void _15_Equals_XV() {
+	assertEquals("solution(15) should equal to XV", "XV", conversion.solution(15));
+    }
+    
+    @Test
+    public void _16_Equals_XVI() {
+	assertEquals("solution(16) should equal to XVI", "XVI", conversion.solution(16));
+    }
+    
+    @Test
+    public void _19_Equals_XIX() {
+	assertEquals("solution(19) should equal to XIX", "XIX", conversion.solution(19));
+    }
+    
+    @Test
+    public void _20_Equals_XX() {
+	assertEquals("solution(20) should equal to XX", "XX", conversion.solution(20));
+    }
+    
+    @Test
+    public void _21_Equals_XXI() {
+	assertEquals("solution(21) should equal to XXI", "XXI", conversion.solution(21));
+    }
+    
+    @Test
+    public void _24_Equals_XXIV() {
+	assertEquals("solution(24) should equal to XXIV", "XXIV", conversion.solution(24));
+    }
+    
+    @Test
+    public void _25_Equals_XXV() {
+	assertEquals("solution(25) should equal to XXV", "XXV", conversion.solution(25));
     }
 
 }
