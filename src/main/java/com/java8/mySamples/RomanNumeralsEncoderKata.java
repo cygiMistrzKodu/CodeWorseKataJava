18e40f2f886c28af2c0e1ef981963b875d3fd4b5
JacekMistrzuKoder <jacek301@gmail.com>
2017-09-27 01:04:46 +0200

    Roman Numerals Encoder Kata 3 Final

--- src/main/java/com/java8/mySamples/RomanNumeralsEncoderKata.java
+++ src/main/java/com/java8/mySamples/RomanNumeralsEncoderKata.java
@@ -16,21 +16,72 @@ public class RomanNumeralsEncoderKata {
 
 	romanNumber = "";
 	
-	
-	
-	if (n >= 20 && n <= 30) {
+	if (n >= 1000 && n <= 3999) {
 
-	    IntStream.rangeClosed(1, n/10).forEach(index -> romanNumber += romanNumberSymbol.get(10));
-	    
-	    n = n - 20;
+	    IntStream.rangeClosed(1, n / 1000).forEach(index -> romanNumber += romanNumberSymbol.get(1000));
+	    n = n - (n / 1000) * 1000;
 
 	}
 	
+	
+	if (n >= 900 && n <= 999) {
+	    
+	    romanNumber += romanNumberSymbol.get(100);
+	    romanNumber += romanNumberSymbol.get(1000);
+	    n = n - 900;
 
-	if (n >= 10 && n <= 19) {
+	}
+	
+	if (n >= 600 && n <= 899) {
+
+	    romanNumber += romanNumberSymbol.get(500);
+	    n = n - 500;
+
+	}
+	
+	if (n >= 400 && n <= 599) {
+
+	    romanNumber += romanNumberSymbol.get(100);
+	    romanNumber += romanNumberSymbol.get(500);
+	    n = n - 400;
+
+	}
+
+	if (n >= 100 && n <= 399) {
+
+	    IntStream.rangeClosed(1, n / 100).forEach(index -> romanNumber += romanNumberSymbol.get(100));
+	    n = n - (n / 100) * 100;
+
+	}
+
+	if (n >= 90 && n <= 99) {
 
 	    romanNumber += romanNumberSymbol.get(10);
-	    n = n - 10;
+	    romanNumber += romanNumberSymbol.get(100);
+	    n = n - 90;
+
+	}
+
+	if (n >= 50 && n <= 89) {
+
+	    romanNumber += romanNumberSymbol.get(50);
+	    n = n - 50;
+
+	}
+
+	if (n >= 40 && n <= 59) {
+
+	    romanNumber += romanNumberSymbol.get(10);
+	    romanNumber += romanNumberSymbol.get(50);
+	    n = n - 40;
+
+	}
+
+	if (n >= 10 && n <= 39) {
+
+	    IntStream.rangeClosed(1, n / 10).forEach(index -> romanNumber += romanNumberSymbol.get(10));
+
+	    n = n - (n / 10) * 10;
 
 	}
 
--- src/test/java/com/java8/mySamples/RomanNumeralsEncoderKataTest.java
+++ src/test/java/com/java8/mySamples/RomanNumeralsEncoderKataTest.java
@@ -98,5 +98,190 @@ public class RomanNumeralsEncoderKataTest {
     public void _25_Equals_XXV() {
 	assertEquals("solution(25) should equal to XXV", "XXV", conversion.solution(25));
     }
+    
+    @Test
+    public void _30_Equals_XXIX() {
+	assertEquals("solution(29) should equal to XXIX", "XXIX", conversion.solution(29));
+    }
+    
+    @Test
+    public void _30_Equals_XXX() {
+	assertEquals("solution(30) should equal to XXX", "XXX", conversion.solution(30));
+    }
+    
+    @Test
+    public void _31_Equals_XXXI() {
+	assertEquals("solution(31) should equal to XXXI", "XXXI", conversion.solution(31));
+    }
+    
+    @Test
+    public void _39_Equals_XXXIX() {
+	assertEquals("solution(39) should equal to XXXIX", "XXXIX", conversion.solution(39));
+    }
+    
+    @Test
+    public void _40_Equals_XL() {
+	assertEquals("solution(40) should equal to XL", "XL", conversion.solution(40));
+    }
+    
+    @Test
+    public void _41_Equals_XLI() {
+	assertEquals("XLI", conversion.solution(41));
+    }
+    
+    @Test
+    public void _48_Equals_XLVIII() {
+	assertEquals("XLVIII", conversion.solution(48));
+    }
+    
+    
+    @Test
+    public void _50_Equals_XL() {
+	assertEquals("L", conversion.solution(50));
+    }
+    
+    @Test
+    public void _51_Equals_LI() {
+	assertEquals("LI", conversion.solution(51));
+    }
+    
+    @Test
+    public void _55_Equals_LV() {
+	assertEquals("LV", conversion.solution(55));
+    }
+    
+    @Test
+    public void _59_Equals_LIX() {
+	assertEquals("LIX", conversion.solution(59));
+    }
+    
+    @Test
+    public void _60_Equals_LX() {
+	assertEquals("LX", conversion.solution(60));
+    }
+    
+    @Test
+    public void _88_Equals_LXXXVIII() {
+	assertEquals("LXXXVIII", conversion.solution(88));
+    }
+    
+    @Test
+    public void _90_Equals_LXL() {
+	assertEquals("XC", conversion.solution(90));
+    }
+    
+    @Test
+    public void _91_Equals_XCI() {
+	assertEquals("XCI", conversion.solution(91));
+    }
+    
+    @Test
+    public void _99_Equals_XCIX() {
+	assertEquals("XCIX", conversion.solution(99));
+    }
+    
+    @Test
+    public void _100_Equals_C() {
+	assertEquals("C", conversion.solution(100));
+    }
+    
+    @Test
+    public void _101_Equals_CI() {
+	assertEquals("CI", conversion.solution(101));
+    }
+    
+    @Test
+    public void _150_Equals_CL() {
+	assertEquals("CL", conversion.solution(150));
+    }
+    
+    @Test
+    public void _230_Equals_CCXXX() {
+	assertEquals("CCXXX", conversion.solution(230));
+    }
+    
+    @Test
+    public void _390_Equals_CCCXC() {
+	assertEquals("CCCXC", conversion.solution(390));
+    }
+    
+    @Test
+    public void _400_Equals_CD() {
+	assertEquals("CD", conversion.solution(400));
+    }
+    
+    @Test
+    public void _401_Equals_CDI() {
+	assertEquals("CDI", conversion.solution(401));
+    }
+    
+    @Test
+    public void _449_Equals_CDXLIX() {
+	assertEquals("CDXLIX", conversion.solution(449));
+    }
+    
+    @Test
+    public void _600_Equals_DC() {
+	assertEquals("DC", conversion.solution(600));
+    }
+    
+    @Test
+    public void _759_Equals_DCCLIX() {
+	assertEquals("DCCLIX", conversion.solution(759));
+    }
+    
+    @Test
+    public void _899_Equals_DCCCXCIX() {
+	assertEquals("DCCCXCIX", conversion.solution(899));
+    }
+    
+    @Test
+    public void _900_Equals_CM() {
+	assertEquals("CM", conversion.solution(900));
+    }
+    
+    @Test
+    public void _901_Equals_CMI() {
+	assertEquals("CMI", conversion.solution(901));
+    }
+    
+    @Test
+    public void _999_Equals_CMXCIX() {
+	assertEquals("CMXCIX", conversion.solution(999));
+    }
+    
+    @Test
+    public void _1000_Equals_M() {
+	assertEquals("M", conversion.solution(1000));
+    }
+    
+    @Test
+    public void _1001_Equals_MI() {
+	assertEquals("MI", conversion.solution(1001));
+    }
+    
+    @Test
+    public void _1666_Equals_MDCLXVI() {
+	assertEquals("MDCLXVI", conversion.solution(1666));
+    }
+    
+    @Test
+    public void _2004_Equals_MMIV() {
+	assertEquals("MMIV", conversion.solution(2004));
+    }
+    
+    @Test
+    public void _3000_Equals_MMM() {
+	assertEquals("MMM", conversion.solution(3000));
+    }
+    
+    @Test
+    public void _3999_Equals_MMMCMXCIX() {
+	assertEquals("MMMCMXCIX", conversion.solution(3999));
+    }
+    
+
+
+
 
 }
